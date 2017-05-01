350857f3a0128d0f9aca4889b121cc7261f5d2dd
Chance Hollis <chancehollis@gmail.com>
2017-05-01 15:41:53 -0600

    Worked on AllRecipes.java. Added RecipeAdapter.java and item_recipe.xml for displaying recipes

--- MyApplication/app/src/main/java/assignment/jpriem/com/androidcookbook/AllRecipes.java
+++ MyApplication/app/src/main/java/assignment/jpriem/com/androidcookbook/AllRecipes.java
@@ -2,20 +2,43 @@ package assignment.jpriem.com.androidcookbook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Matthew Balderas on 5/1/2017.
  */
 
 public class AllRecipes extends Activity {
-
+    private ArrayList<Recipe> recipes = new ArrayList<>();
+    private RecipeAdapter recipeAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the view from new_activity.xml
         setContentView(R.layout.all_recipes);
+        addRecipeName();
+        recipeAdapter = new RecipeAdapter(this, recipes);
+        ListView recipesView = (ListView)findViewById(R.id.RecipesView);
+        recipesView.setAdapter(recipeAdapter);
     }
+
+    public void addRecipeName()
+    {
+//        recipeNames.add("Cake");
+//        recipeNames.add("Chocolate Cake");
+//        recipeNames.add("Chocolate Chip Cookies");
+//        recipeNames.add("Brownies");
+//        recipeNames.add("Snickerdoodles");
+//        recipeNames.add("Calzones");
+//        recipeNames.add("Fried Rice");
+
+    }
+
 
 
 }

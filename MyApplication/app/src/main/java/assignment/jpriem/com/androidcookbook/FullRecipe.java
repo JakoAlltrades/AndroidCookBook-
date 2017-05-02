fadefd47ec4462b995a64623b5fa9b3ab7cb1acb
Chance Hollis <chancehollis@gmail.com>
2017-05-02 15:32:17 -0600

    displayRecipe changes

--- MyApplication/app/src/main/java/assignment/jpriem/com/androidcookbook/FullRecipe.java
+++ MyApplication/app/src/main/java/assignment/jpriem/com/androidcookbook/FullRecipe.java
@@ -2,6 +2,12 @@ package assignment.jpriem.com.androidcookbook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by chance on 5/2/2017.
@@ -10,9 +16,15 @@ import android.os.Bundle;
 public class FullRecipe extends Activity {
 
     private Recipe recipe;
+    private ArrayAdapter<String> ingredientsAdapter;
     public FullRecipe(Recipe recipe)
     {
         this.recipe = recipe;
+    }
+
+    public FullRecipe()
+    {
+
     }
 
     @Override
@@ -21,7 +33,28 @@ public class FullRecipe extends Activity {
         // Get the view from new_activity.xml
         setContentView(R.layout.display_recipe);
 
+    }
 
+    private void displayRecipe()
+    {
+        if (recipe != null)
+        {
+            TextView nameV = (TextView)findViewById(R.id.recipeName);
+            TextView cookTime = (TextView)findViewById(R.id.cookTime);
+            TextView descV = (TextView)findViewById(R.id.recipeDescription);
+            TextView instructions = (TextView)findViewById(R.id.recipeDescription);
+            ListView ingredients = (ListView)findViewById(R.id.recipeIngredientsList);
 
+        }
+    }
+
+    private void SetUpIngredientsList()
+    {
+        ArrayList<Ingredient> ings = recipe.getIngredients();
+        String[] ingStrings = new String[ings.size()];
+        for(int j = 0; j < ings.size(); ++j)
+        {
+            ingStrings[j] = ings.get(j).toString();
+        }
     }
 }
